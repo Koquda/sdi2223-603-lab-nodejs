beb374f45156f937c0535df526eb5fe3339d5230
Koquda <uo282874@uniovi.es>
2023-03-18 12:22:41 +0100

    SDI-2223-603-7.3-División de las rutas en módulos, peticiones GET y parámetros.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -4,20 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-app.get("/songs", function(req,res){
-  console.log("depurar aqui")
-  res.send("Lista de canciones")
-})
-
-app.get("/singers", function (req, res) {
-  console.log("depurar aqui")
-  res.send("Lista de cantantes")
-});
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+require("./routes/songs.js")(app);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
---
+++ musicstoreapp/routes/songs.js
@@ -0,0 +1,26 @@
+module.exports = function(app) {
+    app.get("/songs", function(req, res) {
+        let response = ""
+        if (req.query.title != null && typeof(req.query.title) != "undefined")
+            response = 'Titulo: ' + req.query.title + '<br>'
+        if (req.query.author != null && typeof(req.query.author) != "undefined")
+            response += 'Author: ' + req.query.author
+
+        res.send(response)
+    });
+
+    app.get('/add', function(req, res) {
+        let response = parseInt(req.query.num1) + parseInt(req.query.num2);
+        res.send(String(response))
+    });
+
+    app.get('/songs/:id', function(req, res) {
+        let response = 'id: ' + req.params.id;
+        res.send(response);
+    });
+    app.get('/songs/:kind/:id', function(req, res) {
+        let response = 'id: ' + req.params.id + '<br>'
+            + 'Tipo de música: ' + req.params.kind;
+        res.send(response);
+    });
+};
