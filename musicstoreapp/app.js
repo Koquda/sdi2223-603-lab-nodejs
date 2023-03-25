5c0df6360b8db6b6346ee108746b0aa121700da6
Koquda <uo282874@uniovi.es>
2023-03-25 17:16:05 +0100

    SDI-2223-603-8.1-Acceso a datos de Mongo desde Node.js.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -15,8 +15,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-require("./routes/songs.js")(app);
 require("./routes/authors.js")(app);
+
+// MongoBD
+const { MongoClient } = require("mongodb");
+const url = 'mongodb+srv://admin:campina@musicstoreapp.fakbn46.mongodb.net/?retryWrites=true&w=majority';
+app.set('connectionStrings',url)
+require("./routes/songs.js")(app, MongoClient)
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- musicstoreapp/routes/songs.js
+++ musicstoreapp/routes/songs.js
@@ -1,4 +1,4 @@
-module.exports = function(app) {
+module.exports = function(app, MongoClient) {
     app.get("/songs", function(req, res) {
         let songs = [{
             "title": "Blank space",
@@ -23,12 +23,25 @@ module.exports = function(app) {
     });
 
     app.post('/songs/add', function (req, res) {
-        let response = "Canción agregada: " + req.body.title + "<br>"
-            + " genero: " + req.body.kind + "<br>"
-            + " precio: " + req.body.price
-
-        res.send(response)
-    })
+        let song = {
+            title: req.body.title,
+            kind: req.body.kind,
+            price: req.body.price
+        }
+        MongoClient.connect(app.get('connectionStrings'), function (err, dbClient) {
+            if (err) {
+                res.send("Error de conexión: " + err);
+            } else {
+                const database = dbClient.db("musicStore");
+                const collectionName = 'songs';
+                const songsCollection = database.collection(collectionName);
+                songsCollection.insertOne(song)
+                    .then(result => res.send("canción añadida id: " + result.insertedId))
+                    .then(() => dbClient.close())
+                    .catch(err => res.send("Error al insertar " + err));
+            }
+        });
+    });
 
     app.get('/songs/:id', function(req, res) {
         let response = 'id: ' + req.params.id;
