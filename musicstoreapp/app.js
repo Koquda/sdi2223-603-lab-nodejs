1610c5484a507179805c3085975ab9d6cbdb6d88
Koquda <uo282874@uniovi.es>
2023-03-26 17:22:12 +0200

    SDI-2223-603-8.3-Subida a ficheros y gestión de colecciones.

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -8,6 +8,12 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var app = express();
+let fileUpload = require('express-fileupload');
+app.use(fileUpload({
+  limits: { fileSize: 50 * 1024 * 1024 },
+  createParentPath: true
+}));
+app.set('uploadPath', __dirname)
 
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
