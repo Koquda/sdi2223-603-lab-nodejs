c3f06021f1cbe5577e24653cb9810a94d49e149c
Koquda <uo282874@uniovi.es>
2023-03-30 13:01:38 +0200

    SDI-2223-603-9.3-Mensajes y alertas.

--- musicstoreapp/routes/users.js
+++ musicstoreapp/routes/users.js
@@ -16,9 +16,12 @@ module.exports = function (app, usersRepository) {
       password: securePassword
     }
     usersRepository.insertUser(user).then(userId => {
-      res.redirect("/users/login")
+      res.redirect("/users/login" + "?message=Nuevo usuario registrado." +
+        "&messageType=alert-info")
     }).catch(error => {
-      res.send("Error al insertar el usuario");
+      res.send("Error al insertar el usuario"+
+          "?message=Se ha producido un error al registrar el usuario." +
+          "&messageType=alert-danger");
     });
   });
 
@@ -36,14 +39,20 @@ module.exports = function (app, usersRepository) {
     usersRepository.findUser(filter, options).then(user => {
       if (user == null) {
         req.session.user = null;
-        res.send("Usuario on identificado")
+        //res.send("Usuario on identificado")
+        res.redirect("/users/login" +
+            "?message=Email o password incorrecto"+
+            "&messageType=alert-danger ");
       } else {
         req.session.user = user.email;
         res.redirect("/publications")
       }
     }).catch(error => {
       req.session.user = null;
-      res.send("Se ha producido un error al buscar el usuario: " + error)
+      //res.send("Se ha producido un error al buscar el usuario: " + error)
+      res.redirect("/users/login" +
+          "?message=Se ha producido un error al buscar el usuario"+
+          "&messageType=alert-danger ");
     })
   })
 
