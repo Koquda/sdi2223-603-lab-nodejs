9925a9be6dff4c35d72e060edcfe6ecda0a5be31
Alejandro Campa Martínez <uo282874@uniovi.es>
2023-04-24 22:46:10 +0200

    SDI-2223-603-SW-11.1-Servicio-SOAP-Server.

---
+++ sdi2223-603-lab-soap-sw-server/sdi2223-603-lab-soap-sw-server/src/main/java/com/uniovi/sdi/Sdi2223603LabSoapSwServerApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223603LabSoapSwServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223603LabSoapSwServerApplication.class, args);
+    }
+
+}
